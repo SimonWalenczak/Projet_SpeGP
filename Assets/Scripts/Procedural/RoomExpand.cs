1d8769188312017e65e8abe51a2f5313667d3757
Forstork <simon.walenczak@lacatholille.fr>
2024-11-22 18:17:37 +0100

    [DEV] Refacto RoomExpand Part 1

--- Assets/Scripts/Procedural/RoomExpand.cs
+++ Assets/Scripts/Procedural/RoomExpand.cs
@@ -37,6 +37,13 @@ namespace Procedural
 
         private float _regenerationStep;
         private float _regenerationStepSavedValue;
+        
+        int requiredWallCountX;
+        int requiredWallCountY;
+        float scaleX;
+        float scaleY;
+        int currentWallIndex;
+
 
         private List<GameObject> Walls = new List<GameObject>();
         private List<GameObject> Pillars = new List<GameObject>();
@@ -102,150 +109,135 @@ namespace Procedural
 
         private void CreateWalls()
         {
-            foreach (var wall in Walls)
+            requiredWallCountX = Mathf.Max(1, (int)(_roomSize.x / _wallSize.x));
+            requiredWallCountY = Mathf.Max(1, (int)(_roomSize.y / _wallSize.y));
+            scaleX = (_roomSize.x / requiredWallCountX) / _wallSize.x;
+            scaleY = (_roomSize.y / requiredWallCountY) / _wallSize.y;
+
+            currentWallIndex = 0;
+
+            GameObject GetOrCreateWall(Vector3 position, Quaternion rotation, Vector3 scale)
             {
-                Destroy(wall);
-            }
-
-            Walls.Clear();
-
-            foreach (var pillar in Pillars)
-            {
-                Destroy(pillar);
-            }
-
-            Pillars.Clear();
-
-            int wallCountX = Mathf.Max(1, (int)(_roomSize.x / _wallSize.x));
-            int wallCountY = Mathf.Max(1, (int)(_roomSize.y / _wallSize.y));
-            float scaleX = (_roomSize.x / wallCountX) / _wallSize.x;
-            float scaleY = (_roomSize.y / wallCountY) / _wallSize.y;
-
-            for (int i = 0; i < wallCountX; i++)
-            {
-                Vector3 positionX = transform.position + new Vector3(-_roomSize.x / 2 + _wallSize.x * scaleX / 2 + i * scaleX * _wallSize.x, 0, +_roomSize.y / 2);
-                Quaternion rotationX = transform.rotation;
-                Vector3 localScaleX = new Vector3(scaleX, 1, 1);
-
-                GameObject currentWallX = null;
-
-                currentWallX = Instantiate(_roomMeshes.WallMesh, positionX, rotationX);
-
-                currentWallX.transform.localScale = localScaleX;
-                currentWallX.transform.parent = transform;
-                Walls.Add(currentWallX);
-
-                if (i < wallCountX - 1)
+                GameObject wall;
+                if (currentWallIndex < Walls.Count)
                 {
-                    Vector3 pillarPosition = positionX + new Vector3(scaleX * _wallSize.x / 2, 0, 0);
-                    GameObject pillar = Instantiate(_roomMeshes.PillarMesh, pillarPosition, Quaternion.identity);
-                    pillar.transform.localScale = new Vector3(1, 1, scaleY);
+                    wall = Walls[currentWallIndex];
+                    wall.transform.position = position;
+                    wall.transform.rotation = rotation;
+                    wall.transform.localScale = scale;
+                }
+                else
+                {
+                    wall = Instantiate(_roomMeshes.WallMesh, position, rotation);
+                    wall.transform.localScale = scale;
+                    wall.transform.parent = transform;
+                    Walls.Add(wall);
+                }
+
+                currentWallIndex++;
+                return wall;
+            }
+
+            for (int i = 0; i < requiredWallCountX; i++)
+            {
+                Vector3 position = transform.position + new Vector3(-_roomSize.x / 2 + _wallSize.x * scaleX / 2 + i * scaleX * _wallSize.x, 0, +_roomSize.y / 2);
+                Quaternion rotation = Quaternion.identity;
+                Vector3 scale = new Vector3(scaleX, 1, 1);
+                GetOrCreateWall(position, rotation, scale);
+            }
+
+            for (int i = 0; i < requiredWallCountX; i++)
+            {
+                Vector3 position = transform.position + new Vector3(-_roomSize.x / 2 + _wallSize.x * scaleX / 2 + i * scaleX * _wallSize.x, 0, -_roomSize.y / 2);
+                Quaternion rotation = Quaternion.identity;
+                Vector3 scale = new Vector3(scaleX, 1, 1);
+                GetOrCreateWall(position, rotation, scale);
+            }
+
+            for (int i = 0; i < requiredWallCountY; i++)
+            {
+                Vector3 position = transform.position + new Vector3(-_roomSize.x / 2, 0, -_roomSize.y / 2 + _wallSize.y * scaleY / 2 + i * scaleY * _wallSize.y);
+                Quaternion rotation = Quaternion.Euler(0, 90, 0);
+                Vector3 scale = new Vector3(scaleY, 1, 1);
+                GetOrCreateWall(position, rotation, scale);
+            }
+
+            for (int i = 0; i < requiredWallCountY; i++)
+            {
+                Vector3 position = transform.position + new Vector3(+_roomSize.x / 2, 0, -_roomSize.y / 2 + _wallSize.y * scaleY / 2 + i * scaleY * _wallSize.y);
+                Quaternion rotation = Quaternion.Euler(0, -90, 0);
+                Vector3 scale = new Vector3(scaleY, 1, 1);
+                GetOrCreateWall(position, rotation, scale);
+            }
+
+            for (int i = currentWallIndex; i < Walls.Count; i++)
+            {
+                Destroy(Walls[i]);
+            }
+
+            Walls.RemoveRange(currentWallIndex, Walls.Count - currentWallIndex);
+
+            CreateCornerAndIntermediatePillars(scaleX, scaleY, requiredWallCountX, requiredWallCountY);
+        }
+
+        private void CreateCornerAndIntermediatePillars(float scaleX, float scaleY, int countX, int countY)
+        {
+            int currentPillarIndex = 0;
+
+            GameObject GetOrCreatePillar(Vector3 position, Vector3 scale)
+            {
+                GameObject pillar;
+                if (currentPillarIndex < Pillars.Count)
+                {
+                    pillar = Pillars[currentPillarIndex];
+                    pillar.transform.position = position;
+                    pillar.transform.localScale = scale;
+                }
+                else
+                {
+                    pillar = Instantiate(_roomMeshes.PillarMesh, position, Quaternion.identity);
+                    pillar.transform.localScale = scale;
                     pillar.transform.parent = transform;
                     Pillars.Add(pillar);
                 }
+
+                currentPillarIndex++;
+                return pillar;
             }
 
-            for (int j = 0; j < wallCountX; j++)
+            Vector3[] cornerPositions = new Vector3[]
             {
-                Vector3 positionY = transform.position + new Vector3(-_roomSize.x / 2 + _wallSize.x * scaleX / 2 + j * scaleX * _wallSize.x, 0, +_roomSize.y / 2 - _roomSize.y);
-                Quaternion rotationY = transform.rotation;
-                Vector3 localScaleY = new Vector3(scaleX, 1, 1);
+                transform.position + new Vector3(-_roomSize.x / 2, 0, +_roomSize.y / 2),
+                transform.position + new Vector3(-_roomSize.x / 2, 0, -_roomSize.y / 2),
+                transform.position + new Vector3(+_roomSize.x / 2, 0, +_roomSize.y / 2),
+                transform.position + new Vector3(+_roomSize.x / 2, 0, -_roomSize.y / 2)
+            };
 
-                GameObject currentWallY = null;
-                currentWallY = Instantiate(_roomMeshes.WallMesh, positionY, rotationY);
-
-                currentWallY.transform.localScale = localScaleY;
-                currentWallY.transform.parent = transform;
-                Walls.Add(currentWallY);
-
-                if (j < wallCountX - 1)
-                {
-                    Vector3 pillarPosition = positionY + new Vector3(scaleX * _wallSize.x / 2, 0, 0);
-                    GameObject pillar = Instantiate(_roomMeshes.PillarMesh, pillarPosition, Quaternion.identity);
-                    pillar.transform.localScale = new Vector3(1, 1, scaleY);
-                    pillar.transform.parent = transform;
-                    Pillars.Add(pillar);
-                }
-            }
-
-            // Create left wall
-            for (int k = 0; k < wallCountY; k++)
+            foreach (var position in cornerPositions)
             {
-                Vector3 positionLeft = transform.position + new Vector3(-_roomSize.x / 2, 0, -_roomSize.y / 2 + _wallSize.y * scaleY / 2 + k * scaleY * _wallSize.y);
-                Quaternion rotationLeft = Quaternion.Euler(0, 90, 0); // Rotate 90 degrees for left wall
-                Vector3 localScaleLeft = new Vector3(scaleY, 1, 1);
-
-                GameObject currentWallLeft = null;
-
-                currentWallLeft = Instantiate(_roomMeshes.WallMesh, positionLeft, rotationLeft);
-
-                currentWallLeft.transform.localScale = localScaleLeft;
-                currentWallLeft.transform.parent = transform;
-                Walls.Add(currentWallLeft);
-
-                if (k < wallCountY - 1)
-                {
-                    Vector3 pillarPosition = positionLeft + new Vector3(0, 0, scaleY * _wallSize.y / 2);
-                    GameObject pillar = Instantiate(_roomMeshes.PillarMesh, pillarPosition, Quaternion.identity);
-                    pillar.transform.localScale = new Vector3(scaleX, 1, 1);
-                    pillar.transform.parent = transform;
-                    Pillars.Add(pillar);
-                }
+                GetOrCreatePillar(position, new Vector3(scaleX, 1, scaleY));
             }
 
-            // Create right wall
-            for (int l = 0; l < wallCountY; l++)
+            for (int i = 1; i < countX; i++)
             {
-                Vector3 positionRight = transform.position +
-                                        new Vector3(+_roomSize.x / 2, 0,
-                                            -_roomSize.y / 2 + _wallSize.y * scaleY / 2 + l * scaleY * _wallSize.y);
-                Quaternion rotationRight = Quaternion.Euler(0, -90, 0); // Rotate -90 degrees for right wall
-                Vector3 localScaleRight = new Vector3(scaleY, 1, 1);
-
-                GameObject currentWallRight = null;
-
-
-                currentWallRight = Instantiate(_roomMeshes.WallMesh, positionRight, rotationRight);
-
-                currentWallRight.transform.localScale = localScaleRight;
-                currentWallRight.transform.parent = transform;
-                Walls.Add(currentWallRight);
-
-                if (l < wallCountY - 1)
-                {
-                    Vector3 pillarPosition = positionRight + new Vector3(0, 0, scaleY * _wallSize.y / 2);
-                    GameObject pillar = Instantiate(_roomMeshes.PillarMesh, pillarPosition, Quaternion.identity);
-                    pillar.transform.localScale = new Vector3(scaleX, 1, 1);
-                    pillar.transform.parent = transform;
-                    Pillars.Add(pillar);
-                }
+                float offsetX = -_roomSize.x / 2 + i * (_roomSize.x / countX);
+                GetOrCreatePillar(transform.position + new Vector3(offsetX, 0, +_roomSize.y / 2), new Vector3(scaleX, 1, scaleY));
+                GetOrCreatePillar(transform.position + new Vector3(offsetX, 0, -_roomSize.y / 2), new Vector3(scaleX, 1, scaleY));
             }
 
-            // Create corner pillars
-            Vector3 cornerPosition1 = transform.position + new Vector3(-_roomSize.x / 2, 0, +_roomSize.y / 2);
-            Vector3 cornerPosition2 = transform.position + new Vector3(-_roomSize.x / 2, 0, -_roomSize.y / 2);
-            Vector3 cornerPosition3 = transform.position + new Vector3(+_roomSize.x / 2, 0, +_roomSize.y / 2);
-            Vector3 cornerPosition4 = transform.position + new Vector3(+_roomSize.x / 2, 0, -_roomSize.y / 2);
+            for (int i = 1; i < countY; i++)
+            {
+                float offsetY = -_roomSize.y / 2 + i * (_roomSize.y / countY);
+                GetOrCreatePillar(transform.position + new Vector3(-_roomSize.x / 2, 0, offsetY), new Vector3(scaleX, 1, scaleY));
+                GetOrCreatePillar(transform.position + new Vector3(+_roomSize.x / 2, 0, offsetY), new Vector3(scaleX, 1, scaleY));
+            }
 
-            GameObject cornerPillar1 = Instantiate(_roomMeshes.PillarMesh, cornerPosition1, Quaternion.identity);
-            GameObject cornerPillar2 = Instantiate(_roomMeshes.PillarMesh, cornerPosition2, Quaternion.identity);
-            GameObject cornerPillar3 = Instantiate(_roomMeshes.PillarMesh, cornerPosition3, Quaternion.identity);
-            GameObject cornerPillar4 = Instantiate(_roomMeshes.PillarMesh, cornerPosition4, Quaternion.identity);
+            for (int i = currentPillarIndex; i < Pillars.Count; i++)
+            {
+                Destroy(Pillars[i]);
+            }
 
-            cornerPillar1.transform.localScale = new Vector3(scaleX, 1, scaleY);
-            cornerPillar2.transform.localScale = new Vector3(scaleX, 1, scaleY);
-            cornerPillar3.transform.localScale = new Vector3(scaleX, 1, scaleY);
-            cornerPillar4.transform.localScale = new Vector3(scaleX, 1, scaleY);
-
-            cornerPillar1.transform.parent = transform;
-            cornerPillar2.transform.parent = transform;
-            cornerPillar3.transform.parent = transform;
-            cornerPillar4.transform.parent = transform;
-
-            Pillars.Add(cornerPillar1);
-            Pillars.Add(cornerPillar2);
-            Pillars.Add(cornerPillar3);
-            Pillars.Add(cornerPillar4);
+            Pillars.RemoveRange(currentPillarIndex, Pillars.Count - currentPillarIndex);
         }
 
         public void GenerateDoorsRandomPosition()
@@ -306,7 +298,6 @@ namespace Procedural
         {
             if (Input.GetMouseButtonDown(0))
             {
-                // Détection du handler via un raycast
                 Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
                 if (Physics.Raycast(ray, out RaycastHit hit))
                 {
@@ -321,18 +312,15 @@ namespace Procedural
 
             if (Input.GetMouseButton(0) && _selectedHandle != null)
             {
-                // Obtenir la position actuelle de la souris sur le même plan
                 Vector3 currentMousePosition = GetMousePositionOnPlane(_dragStartPosition, Vector3.up);
                 Vector3 delta = currentMousePosition - _dragStartPosition;
 
                 if (_selectedHandle == ExpandHandleLength)
                 {
-                    // Déplacer le handle en X
                     _selectedHandle.position = _handleStartPosition + new Vector3(delta.x, 0, 0);
                 }
                 else if (_selectedHandle == ExpandHandleWidth)
                 {
-                    // Déplacer le handle en Z
                     _selectedHandle.position = _handleStartPosition + new Vector3(0, 0, delta.z);
                 }
             }
